914b46b6f3a770be09bdb29a8838765c57faf4af
(Michael-Start) <(mstart911@gmail.com)>
2022-11-01 23:37:38 -0700

    Bootstrap working

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import {Link} from 'react-router-dom'
 import Image from 'react-bootstrap/Image'
+import Card from 'react-bootstrap/Card'
 
 const List = () => {
     const[cats, setCats] = useState([]);
@@ -20,14 +21,19 @@ const List = () => {
   return (
     <div>
         <h1>All my Favorite Cats!</h1>
+        <div className='list'>
+        <Card style = {{width:'18rem'}} className= 'text-center'>
         {cats.map((item,index)=>(
-            <div className='card' key= {index}><Image className='img-fluid' src={item.picture} alt = 'A floofer'></Image> 
+            <div key= {index}><Image className='img-fluid' src={item.picture} alt = 'A floofer'></Image> 
             <p>{item.caption}</p>
             <Link to = {`/detail/${item._id}`}>Update Kitty</Link>
             </div>
         ))}
+        </Card>
+        <div className='link'>
         <Link to = '/'>Get another cat</Link>
-
+        </div>
+        </div>
     </div>
   )
 }
